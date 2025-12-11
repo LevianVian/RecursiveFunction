f77230f7218989d9fb38e149899ee8f89e20e5df
LevianVian <artalevi4@gmail.com>
2025-12-11 11:57:29 +0700

    Add ProvitRecursive12 class for profit calculation with user input

---
+++ ProvitRecursive12.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ProvitRecursive12 {
+    static double calculateProfit(double balance, int period){
+        if (period == 0)
+            return balance;
+        else
+            return 1.11*calculateProfit(balance, period - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input Balance: ");
+        double initialBalance = input.nextDouble();
+        System.out.println("Input Investment Period: ");
+        int investPeriod = input.nextInt();
+        System.out.println("Balance after "+investPeriod+" year = "+calculateProfit(initialBalance, investPeriod));
+    }
+
+}
